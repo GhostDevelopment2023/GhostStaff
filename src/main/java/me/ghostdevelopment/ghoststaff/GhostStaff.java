83c158d83615cc393f3461a6cb5e9e761bbe1562
GhostAndry <andrearonzoni20062022@gmail.com>
2024-01-26 17:05:50 +0100

    Save

--- src/main/java/me/ghostdevelopment/ghoststaff/GhostStaff.java
+++ src/main/java/me/ghostdevelopment/ghoststaff/GhostStaff.java
@@ -1,12 +1,18 @@
 package me.ghostdevelopment.ghoststaff;
 
+import lombok.Getter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GhostStaff extends JavaPlugin {
 
+    @Getter
+    private GhostStaff instance;
+
     @Override
     public void onEnable() {
+
+        instance=this;
 
         Metrics metrics = new Metrics(this, 20502);
 
@@ -14,6 +20,6 @@ public final class GhostStaff extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        instance=null;
     }
 }
